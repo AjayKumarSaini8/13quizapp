e81f9eadffe5540f7d0c7b29e5f5e7de178b17e0
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-02-12 13:05:38 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-// App.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Welcome, Quiz } from "./components";
-import { Link } from "react-router-dom";
 import { jsQuiz } from "./constants";
 
 function App() {
@@ -11,10 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/quiz" element={<Quiz questions={jsQuiz.questions} />} />
-        <Route path="/quiz" element={<Quiz questions={jsQuiz.questions} />} />
+        <Route path="/quiz/:questionNumber" element={<Quiz questions={jsQuiz.questions} />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
